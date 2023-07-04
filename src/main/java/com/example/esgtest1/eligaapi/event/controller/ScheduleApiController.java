da8ef89f75cc7aa722283a42e4960a43b81292b8
gwangjunho <googunet@gmail.com>
2023-07-04 17:22:24 +0900

    main 수정

--- src/main/java/com/example/esgtest1/eligaapi/event/controller/ScheduleApiController.java
+++ src/main/java/com/example/esgtest1/eligaapi/event/controller/ScheduleApiController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ScheduleApiController {
     @RequestMapping(value = "/")
     public String main() {
+        System.out.println("aaaaa");
         return "index";
     }
 }
